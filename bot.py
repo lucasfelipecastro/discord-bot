bec2355c844698e0146aaa09b56e3deb9cc4f7d5
lucasfelipecastro <lucassantanafelipe@gmail.com>
2025-01-20 12:23:11 -0300

    feat: add a greeting command to the user

--- bot.py
+++ bot.py
@@ -7,5 +7,17 @@ load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 permissions = discord.Intents.default()
+permissions.message_content = True
+permissions.members = True
 bot = commands.Bot(command_prefix='.', intents=permissions)
+
+@bot.command()
+async def hi(ctx:commands.Context):
+    user = ctx.author
+    await ctx.reply(f"Hi, {user.display_name}")
+
+@bot.event
+async def on_ready():
+    print('Ready.')
+
 bot.run(TOKEN)
